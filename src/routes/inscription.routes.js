d0992cc953732b52e5ed00315fe365aa23ed9754
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-11-30 22:00:56 +0100

    Update the incription routes

--- src/routes/inscription.routes.js
+++ src/routes/inscription.routes.js
@@ -5,7 +5,7 @@ import * as inscriptionController from '../controllers/inscription.controller.js
 const router = express.Router();
 
 router.post('/:missionId', authenticate, inscriptionController.createInscription);
-router.get('/mission/:missionId', authenticate, inscriptionController.getMissionInscriptions);
+router.get('/mission/:missionId', authenticate, inscriptionController.getMissionInscriptions); // Route was already correct
 router.get('/user', authenticate, inscriptionController.getUserInscriptions);
 router.put('/:id', authenticate, inscriptionController.updateInscriptionStatus);
 router.delete('/:id', authenticate, inscriptionController.cancelInscription);
